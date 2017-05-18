f08cd6d3c44643cbd4bfefe6b922996adcc0b1d5
Dylan Kelly <dylan@truss.io>
2017-05-18 22:26:20 +1000

    sass loader config

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -140,6 +140,7 @@ module.exports = {
           /\.gif$/,
           /\.jpe?g$/,
           /\.png$/,
+          /\.scss$/,
         ],
         loader: require.resolve('file-loader'),
         options: {
@@ -205,6 +206,12 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+
       // ** STOP ** Are you adding a new loader?
       // Remember to add the new extension(s) to the "file" loader exclusion list.
     ],
